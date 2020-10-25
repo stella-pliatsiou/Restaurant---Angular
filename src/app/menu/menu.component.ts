3628fb602e9d97db367a5a50717f810059b79444
Stella Pliatsiou <pliatsioustella@gmail.com>
2020-10-25 15:24:31 +0200

    'dishdetails'

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-
+import { DishDetails } from '../shared/dishdetails';
 
 const DISHES: Dish[] = [
   {
@@ -44,8 +44,42 @@ const DISHES: Dish[] = [
     price: '2.99',
     description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
   }
+ ];
 
-  ];
+
+ const DISH: DishDetails[]  = [
+  {
+           rating: 5,
+           comment: 'Imagine all the eatables, living in conFusion!',
+           author: 'John Lemon',
+           date: '2012-10-16T17:57:28.556094Z'
+       },
+       {
+           rating: 4,
+           comment: 'Sends anyone to heaven,I wish I could get my mother-in-law to eat it!',
+           author: 'Paul McVites',
+           date: '2014-09-05T17:57:28.556094Z'
+       },
+       {
+           rating: 3,
+           comment: 'Eat it, just eat it!',
+           author: 'Michael Jaikishan',
+           date: '2015-02-13T17:57:28.556094Z'
+       },
+       {
+           rating: 4,
+           comment: 'Ultimate, Reaching for the stars!',
+           author: 'Ringo Starry',
+           date: '2013-12-02T17:57:28.556094Z'
+       },
+       {
+           rating: 2,
+           comment: 'It\'s your birthday, we\'re gonna party!',
+           author: '25 Cent',
+           date: '2011-12-02T17:57:28.556094Z'
+          }
+        ];
+
 
 @Component({
   selector: 'app-menu',
@@ -55,9 +89,18 @@ const DISHES: Dish[] = [
 export class MenuComponent implements OnInit {
 
   dishes = DISHES;
-  
-  selectedDish = DISHES[3];
+  selectedDish = DISHES[0];
 
+
+
+  dishdetails = DISH[0];
+  dishdetails1 = DISH[1];
+  dishdetails2 = DISH[2];
+  dishdetails3 = DISH[3];
+  dishdetails4 = DISH[4];
+
+
+ 
 
   constructor() { }
 
